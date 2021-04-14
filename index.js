6c7aa793422d205efef5e54a8ac47696cf3dcd5a
Simone <simomilanesio5@gmail.com>
2021-04-14 12:43:19 +0200

    errors heroku fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const bcrypt = require('bcrypt');
 const faker = require('faker/locale/it');
 const saltRounds = 10;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 require('dotenv').config();
 
@@ -557,4 +557,4 @@ app.post("/signup", register);
 app.post("/insert", checkCredentials, insBook);
 app.post("/favorite", checkCredentials, favorite);
 
-app.listen(process.env.PORT || port, function () { console.log(`Server started at port: ${port}`) });
+app.listen(port, function () { console.log(`Server started at port: ${port}`) });
